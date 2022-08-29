1ec15ea72d2adbed066033e540aac4f6dbcd885f
Jesus Clemente <jcdeveloper92@gmail.com>
2022-08-29 00:04:39 -0400

    advanced layout 100%

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,13 +1,17 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import nameTrainerSlice, { setNameTrainer } from '../store/slices/nameTrainer.slice'
+import { useSelector } from 'react-redux'
 import PokemonCard from './Pokedex/PokemonCard'
 
 const Pokedex = () => {
 
+  const nameTrainer = useSelector(state => state.nameTrainer)
+
   const [pokemons, setPokemons] = useState()
 
   useEffect(() => {
-    const URL = 'https://pokeapi.co/api/v2/pokemon'
+    const URL = 'https://pokeapi.co/api/v2/pokemon/?limit=32&offset=1'
     axios.get(URL)
       .then(res => setPokemons(res.data))
       .catch(err => console.log(err))
@@ -23,6 +27,18 @@ const Pokedex = () => {
           <div className="box__ball in in--header"></div>
         </div>
       </header>
+      <div className='searchbar'>
+        <h1>
+          Welcome  trainer... <span>{nameTrainer}</span>! you can find your favorite pokemon here!
+        </h1>
+        <div className="searchbar__container">
+          <div className="searchbar__input">
+            <input type="text" />
+            <button type="button">Search</button>
+          </div>
+          <select className="searchbar__select"></select>
+        </div>
+      </div>
       <div className='pokedex__cards'>
         {
           pokemons?.results.map(pokemon => (
@@ -32,6 +48,8 @@ const Pokedex = () => {
             />
           ))
         }
+      </div>
+      <div className="pagination">
       </div>
     </section>
   )
