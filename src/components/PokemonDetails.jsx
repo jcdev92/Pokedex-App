3e38d06f85605ff7dda5559b0ec6d2a1f4e52c1e
Jesus Clemente <jcdeveloper92@gmail.com>
2022-08-31 20:00:03 -0400

    adding styles to the poke details section

--- src/components/PokemonDetails.jsx
+++ src/components/PokemonDetails.jsx
@@ -1,13 +1,97 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const PokemonDetails = () => {
+  const [pokemon, setPokemon] = useState();
+  const { name } = useParams();
 
-  const {name} = useParams()
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then((res) => {
+        setPokemon(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const navigate = useNavigate();
+
+  const handleReset = () => {
+    navigate("/pokedex");
+  };
 
   return (
-    <h1>{name}</h1>
-  )
-}
+    <section className="details">
+      <header className="pokedex__box pokedex__box--header">
+        <img
+          src="src/assets/image11.png"
+          alt="pokemon"
+          className="pokedex__img"
+        />
+        <div className="box__red box__red--header"></div>
+        <div className="box__black box__black--header"></div>
+        <div className="box__ball box__ball--header" onClick={handleReset}>
+          <div className="box__ball in in--header"></div>
+        </div>
+      </header>
+      <article className="pokemon__details">
+        <body className="pokemon__details pokemon__details__body">
+            <img
+              src={pokemon?.sprites.other["official-artwork"]["front_default"]}
+              alt={name}
+            />
+            <h1 className="pokemon__details pokemon__details__body pokemon__details__body__name ">{name}</h1>
+          <ul className="pokemon__details__body pokemon__details__body__list">
+          <li className="">
+              <h3 className="">Weight</h3>
+              <p className="">
+                {pokemon?.types.map((type) => type.type.name)}
+              </p>
+              <h3 className="">Types</h3>
+              <p className="">{pokemon?.weight}</p>
+            </li>  
+            <li className="">
+              <h3 className="">Height</h3>
+              <p className="">{pokemon?.height}</p>
+              <h3 className="">Abilities</h3>
+              <p className="">
+                {pokemon?.abilities.map((ability) => ability.ability.name)}
+              </p>
+            </li>
+          </ul>
+        </body>
+        <footer className="pokemon__details pokemon__details__footer">
+          <h1 className="pokemon__details pokemon__details__footer pokemon__details__footer__name">Stats</h1>
+          <ul className="pokemon__details pokemon__details__footer pokemon__details__footer__list">
+            {pokemon?.stats.map((stat) => (
+              <li className="pokemon__footer pokemon__list-item" key={stat.stat.url}>
+                <h3 className="pokemon__footer pokemon__list-item-title">{stat.stat.name}</h3>
+                <p className="pokemon__footer pokemon__list-item-value">{stat.base_stat}</p>
+              </li>
+            ))}
+          </ul>
+        </footer>
+      </article>
+      <article className="pokemon__moves">
+        <header className="pokemon__moves pokemon__moves__header">
+          <h1 className="pokemon__moves__name">Moves</h1>
+        </header>
+        <body className="pokemon__moves__body">
+          <ul className="pokemon__moves__list">
+            {pokemon?.moves.map((move) => (
+              <li className="pokemon__list-item" key={move.move.url}>
+                <h3 className="pokemon__list-item-title">{move.move.name}</h3>
+              </li>
+            ))}
+          </ul>
+        </body>
+      </article>
+    </section>
+  );
+};
 
-export default PokemonDetails
+export default PokemonDetails;
