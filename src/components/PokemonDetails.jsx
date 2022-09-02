92214551b0a36a05a4dd2ad0324f1105f6fe379b
Jesus Clemente <jcdeveloper92@gmail.com>
2022-09-02 19:22:46 -0400

    advanced 90% pokemon details page completed design

--- src/components/PokemonDetails.jsx
+++ src/components/PokemonDetails.jsx
@@ -24,6 +24,8 @@ const PokemonDetails = () => {
     navigate("/pokedex");
   };
 
+  console.log(pokemon?.types[0].type.name);
+
   return (
     <section className="details">
       <header className="pokedex__box pokedex__box--header">
@@ -39,57 +41,73 @@ const PokemonDetails = () => {
         </div>
       </header>
       <article className="pokemon__details details__img">
-        <div className="pokemon__details pokemon__img">
-              <img src={pokemon?.sprites.other["official-artwork"]["front_default"]}
-              alt={name} />
-        </div> 
+        <div
+          className="pokemon__details pokemon__img"
+          style={{
+            backgroundImage: `var(--color-${pokemon?.types[0].type.name})`,
+          }}
+        >
+          <img
+            src={pokemon?.sprites.other["official-artwork"]["front_default"]}
+            alt={name}
+          />
+        </div>
         <div className="pokemon__details details__body">
-            <h1 className="pokemon__details pokemon__id">{`#${pokemon?.id}`}</h1>
-            <div className="pokemon__details details__body name ">
-              <div className="name__line"></div>
-              <h1>{name}</h1>
-              <div className="name__line"></div>
-            </div>
-            <li className="pokemon__details details__bod list__sizes">
-              <span className="pokemon__details details__bod list__sizes--span">
-                <h3>Height</h3>
-                <p>{pokemon?.height}</p>
-              </span>
-              <span className="pokemon__details details__bod list__sizes--span">
-                <h3>Weight</h3>
-                <p>{pokemon?.weight}</p>
-              </span>
-            </li>  
-            <li className="pokemon__details details__body list__abilities">
-              <span className="pokemon__details details__bod list__abilities--span">
-                <div className="pokemon__details details__body sub__title">
-                  <h3>Types</h3>
-                </div>
-                <div className="pokemon__details details__body type__abilities">
-                  {pokemon?.types.map((type, index) => {
-                    return (
-                      <span key={index} className="pokemon__details sub__span">
-                        <h3>{type.type.name}</h3>
-                      </span>
-                    );
-                  })}
-                </div>
-              </span>
-              <span className="pokemon__details details__bod list__abilities--span">
-                <div className="pokemon__details details__body sub__title">
-                  <h3>Abilities</h3>
-                </div>
-                <div className="pokemon__details details__body type__abilities">
-                  {pokemon?.abilities.map((ability, index) => {
-                    return (
-                      <span key={index} className="pokemon__details sub__span">
-                        <h3>{ability.ability.name}</h3>
-                      </span>
-                    );
-                  })}
-                </div>
-              </span>
-            </li>
+          <h1 className="pokemon__details pokemon__id">{`#${pokemon?.id}`}</h1>
+          <div className="pokemon__details details__body name ">
+            <div className="name__line"></div>
+            <h1>{name}</h1>
+            <div className="name__line"></div>
+          </div>
+          <li className="pokemon__details details__bod list__sizes">
+            <span className="pokemon__details details__bod list__sizes--span">
+              <h3>Height</h3>
+              <p>{pokemon?.height}</p>
+            </span>
+            <span className="pokemon__details details__bod list__sizes--span">
+              <h3>Weight</h3>
+              <p>{pokemon?.weight}</p>
+            </span>
+          </li>
+          <li className="pokemon__details details__body list__abilities">
+            <span className="pokemon__details details__bod list__abilities--span">
+              <div className="pokemon__details details__body sub__title">
+                <h3>Types</h3>
+              </div>
+              <div className="pokemon__details details__body type__abilities">
+                {pokemon?.types.map((type, index) => {
+                  return (
+                    <span
+                      key={index}
+                      className="pokemon__details sub__span sub__span--type"
+                      style={{
+                        backgroundImage: `var(--color-${type.type.name})`,
+                      }}
+                    >
+                      <h3>{type.type.name}</h3>
+                    </span>
+                  );
+                })}
+              </div>
+            </span>
+            <span className="pokemon__details details__bod list__abilities--span">
+              <div className="pokemon__details details__body sub__title">
+                <h3>Abilities</h3>
+              </div>
+              <div className="pokemon__details details__body type__abilities">
+                {pokemon?.abilities.map((ability, index) => {
+                  return (
+                    <span
+                      key={index}
+                      className="pokemon__details sub__span sub__span--ability"
+                    >
+                      <h3>{ability.ability.name}</h3>
+                    </span>
+                  );
+                })}
+              </div>
+            </span>
+          </li>
         </div>
         <footer className="pokemon__details details__footer">
           <div className="pokemon__details details__footer details__footer__name">
@@ -97,12 +115,20 @@ const PokemonDetails = () => {
           </div>
           <ul className="pokemon__details details__footer details__footer__list">
             {pokemon?.stats.map((stat) => (
-              <li className="pokemon__footer pokemon__list-item" key={stat.stat.url}>
+              <li
+                className="pokemon__footer pokemon__list-item"
+                key={stat.stat.url}
+              >
                 <div className="pokemon__footer pokemon__list-item-container">
-                  <h3 className="pokemon__footer pokemon__list-item-title">{stat.stat.name}</h3>
+                  <h3 className="pokemon__footer pokemon__list-item-title">
+                    {stat.stat.name}
+                  </h3>
                   <p className="pokemon__footer pokemon__list-item-value">{`${stat.base_stat} / 150`}</p>
                 </div>
-                <div className="pokemon__footer pokemon__list-item-bar" style={{ width: `${stat.base_stat}%` }}></div>
+                <div
+                  className="pokemon__footer pokemon__list-item-bar"
+                  style={{ width: `${stat.base_stat*0.7}%` }}
+                ></div>
               </li>
             ))}
           </ul>
@@ -110,13 +136,16 @@ const PokemonDetails = () => {
       </article>
       <article className="pokemon__moves">
         <header className="pokemon__moves moves__header">
-          <h1>Moves</h1><div className="title__line"></div>
+          <h1>Moves</h1>
+          <div className="title__line"></div>
         </header>
         <body className="pokemon__moves moves__body">
           <ul className="pokemon__moves moves__list">
             {pokemon?.moves.map((move) => (
               <li className="pokemon__moves list__item" key={move.move.url}>
-                <h3 className="pokemon__move list__item-title">{move.move.name}</h3>
+                <h3 className="pokemon__move list__item-title">
+                  {move.move.name}
+                </h3>
               </li>
             ))}
           </ul>
